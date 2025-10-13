064f256da38df19c729c413ca53e916305d32572
BNRD_10k Bishop <chiherengebenard10@gmail.com>
2025-10-13 14:23:50 +0200

    Initial Commit

---
+++ Carelium Home/Carelium_HomeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Carelium_HomeApp.swift
+//  Carelium Home
+//
+//  Created by Benard Chiherenge on 13/10/2025.
+//
+
+import SwiftUI
+
+@main
+struct Carelium_HomeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
