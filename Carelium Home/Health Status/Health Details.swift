a7c2e3aa595f367545834e9cc8ea2aacb1f6d774
BNRD_10k Bishop <chiherengebenard10@gmail.com>
2025-10-27 12:10:23 +0200

    created dummy info for the health status page

---
+++ Carelium Home/Health Status/Health Details.swift
@@ -0,0 +1,18 @@
+//
+//  Health Details.swift
+//  Carelium Home
+//
+//  Created by Benard Chiherenge on 27/10/2025.
+//
+
+import Foundation
+
+struct HealthDetails {
+    var heartRate:Int
+    var weight:Double 
+    var topBloodPressure :Int
+    var bottomBloodPressure :Int
+    var sleepTime : Double
+    
+}
+var clientStatus = HealthDetails(heartRate: 95, weight: 97.5, topBloodPressure: 125, bottomBloodPressure: 79, sleepTime: 8.5)
