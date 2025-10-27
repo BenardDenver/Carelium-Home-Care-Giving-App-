832ac56b94bfc62e0a5263430e1266e9e18dabe3
BNRD_10k Bishop <chiherengebenard10@gmail.com>
2025-10-27 12:12:31 +0200

    added all separate views to the health status page

--- Carelium Home/Health Status/Health_status-page.swift
+++ Carelium Home/Health Status/Health_status-page.swift
@@ -9,9 +9,41 @@ import SwiftUI
 
 struct Health_Status:View {
     var body: some View {
-        VStack{
-            
+        VStack(alignment:.leading){
+            Text("Health Status").font(.title).bold()
+            VStack(spacing:30){
+                ScrollView(.horizontal){
+                    HStack {
+                        BloodPressure()
+                        SleepTime()
+                    }
+                   
+                }
+                .scrollIndicators(.hidden)
+                ScrollView(.horizontal){
+                    HStack {
+                     Pulse()
+                        Weight()
+                    }
+                   
+                }
+                .scrollIndicators(.hidden)
+            }
+            Spacer()
+            Button{
+                
+            }label: {
+                ZStack{
+                    Rectangle().foregroundColor(.green)
+                        .frame(height: 70)
+                        .padding(20)
+                    Text("Call Caregiver")
+                        .foregroundStyle(.white)
+                        .font(.title)
+                }
+            }
         }
+        .padding()
         .toolbar(.hidden, for: .tabBar)
     }
 }
