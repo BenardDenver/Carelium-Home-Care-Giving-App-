acb03c23d019c933f883b2f5fc0170af5f056482
BNRD_10k Bishop <chiherengebenard10@gmail.com>
2025-10-28 13:00:59 +0200

    created account creation pages

---
+++ Carelium Home/Accounts Setup/Login-page.swift
@@ -0,0 +1,198 @@
+//
+//  Login-page.swift
+//  Carelium Home
+//
+//  Created by Benard Chiherenge on 15/10/2025.
+//
+
+import SwiftUI
+
+struct Login: View {
+    @State var email:String = ""
+    @State var password:String = ""
+    @State var seePassword:Bool = false
+    @State var isResetting:Bool = false
+    
+    var body: some View{
+        NavigationStack{
+            VStack(spacing:20){
+                Text("Welcome Back!")
+                    .font(.largeTitle)
+                    .bold()
+                    .padding()
+                    .offset(y: 70)
+                
+                Spacer()
+                
+                // textfields
+                HStack{
+                    TextField("Email", text: $email)
+                    Button{
+                        
+                    } label:{
+                        Image(systemName: "person.fill")
+                            .foregroundStyle(.gray)
+                            .padding(.trailing, 5)
+                            
+                    }
+                }
+                .padding()
+                .overlay{
+                    RoundedRectangle(cornerRadius: 25)
+                        .stroke(lineWidth: 1)
+                        .foregroundStyle(.orange)
+                    
+                }
+                
+                HStack{
+                    Group{
+                        if seePassword{
+                            TextField("Password", text: $password)
+                        } else{
+                            SecureField("Password", text: $password)
+                        }
+                    }
+                    Button{
+                        seePassword.toggle()
+                    } label:{
+                        Image(systemName: seePassword ? "eye.slash" : "eye")
+                            .foregroundStyle(.gray)
+                    }
+                    
+                    
+                }
+                .padding()
+                .overlay{
+                    RoundedRectangle(cornerRadius: 25)
+                        .stroke(lineWidth: 1)
+                        .foregroundStyle(.orange)
+                    
+                }
+                
+                // forgot password
+                HStack{
+                    Spacer()
+                    Button("Forgot Password"){
+                        isResetting.toggle()
+                    }
+                    .foregroundStyle(.orange)
+                    .padding(.trailing,20)
+                    .sheet(isPresented: $isResetting, onDismiss: dismiss){
+                        ResetPassword(isResetting: $isResetting)
+                            .presentationDetents([.medium, .large])
+                    }
+                }
+                
+                   //Login button
+                Button{
+                    
+                } label: {
+                    ZStack{
+                        RoundedRectangle(cornerRadius: 25)
+                            .fill(.orange)
+                            .frame(width: 350,height:50)
+                        Text("Log in")
+                            .font(.title2)
+                            .foregroundStyle(.white)
+                            .bold()
+                    }
+                }
+                
+                // sign up
+                HStack{
+                    Text("New User?")
+                    NavigationLink("Create Acoount"){
+                        
+                    }
+                    .foregroundStyle(.orange)
+                }
+                
+                // sign with other accounts
+                HStack{
+                    Rectangle()
+                        .frame(width: 150, height: 2)
+                    Text("OR")
+                        .foregroundStyle(.orange)
+                    Rectangle()
+                        .frame(width: 150, height: 2)
+                }
+                HStack{
+                    //Apple button
+                    Button{
+                        
+                    } label: {
+                        ZStack {
+                            Circle().stroke(lineWidth: 1)
+                                .frame(width: 50, height: 50)
+                            Image("apple-logo")
+                                .resizable()
+                                .frame(width: 30, height: 30)
+                                
+                        }
+                        
+                    }
+                    .buttonStyle(.glass)
+                    
+                    //Facebook button
+                    Button{
+                        
+                    } label: {
+                        ZStack {
+                            Circle().stroke(lineWidth: 1)
+                                .frame(width: 50, height: 50)
+                            Image("facebook")
+                                .resizable()
+                                .frame(width: 30, height: 30)
+                        }
+                        
+                    }
+                    .buttonStyle(.glass)
+                    
+                    //Linkedin button
+                    Button{
+                        
+                    } label: {
+                        ZStack {
+                            Circle().stroke(lineWidth: 1)
+                                .frame(width: 50, height: 50)
+                            Image("linkedin")
+                                .resizable()
+                                .frame(width: 30, height: 30)
+                        }
+                        
+                    }
+                    .buttonStyle(.glass)
+                    
+                    //Google button
+                    Button{
+                        
+                    } label: {
+                        ZStack {
+                            Circle().stroke(lineWidth: 1)
+                                .frame(width: 50, height: 50)
+                            Image("google")
+                                .resizable()
+                                .frame(width: 30, height: 30)
+                        }
+                        
+                    }
+                    .buttonStyle(.glass)
+                }
+                Text("Sign in with another account")
+                    .foregroundStyle(.gray)
+                Spacer()
+                 
+            }
+            .padding()
+            
+            
+        }
+        
+    }
+    func dismiss(){
+        
+    }
+}
+#Preview {
+    Login()
+}
---
+++ Carelium Home/Accounts Setup/Signup-page.swift
@@ -0,0 +1,148 @@
+//
+//  Signup-page.swift
+//  Carelium Home
+//
+//  Created by Benard Chiherenge on 15/10/2025.
+//
+
+import SwiftUI
+
+struct Signup:View {
+    @State var firstName:String = ""
+    @State var lastName:String = ""
+    @State var email:String = ""
+    @State var password:String = ""
+    @State var confirmPassword = ""
+    @State var seePassword:Bool = false
+    @State var seeConfirmPassword:Bool = false
+    var body: some View {
+        NavigationStack{
+            VStack(spacing: 20) {
+            //text fields
+                HStack{
+                    TextField("First Name", text: $firstName)
+                        .padding()
+                        .frame(width: 150)
+                        .overlay{
+                            RoundedRectangle(cornerRadius: 25)
+                                .stroke(lineWidth: 1)
+                                .foregroundStyle(.orange)
+                                .frame(height: 50)
+                                
+                        }
+                    Spacer()
+                    TextField("Last Name", text: $lastName)
+                        .padding()
+                        .frame(width: 200)
+                        .overlay{
+                            RoundedRectangle(cornerRadius: 25)
+                                .stroke(lineWidth: 1)
+                                .foregroundStyle(.orange)
+                                .frame(height: 50)
+                                
+                        }
+                }
+                HStack {
+                    TextField("Email", text: $email)
+                        .padding()
+                    Image(systemName: "person.fill")
+                            .foregroundStyle(.gray)
+                            .padding(.trailing, 18)
+                }
+                .overlay{
+                            RoundedRectangle(cornerRadius: 25)
+                                .stroke(lineWidth: 1)
+                                .foregroundStyle(.orange)
+                                .frame(height: 50)
+                            
+                        }
+                
+                
+                //password
+                HStack{
+                    Group{
+                        if seePassword{
+                            TextField("Password", text: $password)
+                        } else{
+                            SecureField("Password", text: $password)
+                        }
+                    }
+                    Button{
+                        seePassword.toggle()
+                    } label:{
+                        Image(systemName: seePassword ? "eye.slash" : "eye")
+                            .foregroundStyle(.gray)
+                    }
+                }
+                .padding()
+                .overlay{
+                    RoundedRectangle(cornerRadius: 25)
+                        .stroke(lineWidth: 1)
+                        .foregroundStyle(.orange)
+                    
+                }
+                
+                //Confirm password
+                HStack{
+                    Group{
+                        if seeConfirmPassword{
+                            TextField("Confirm Password", text: $confirmPassword)
+                        } else{
+                            SecureField("Confirm Password", text: $confirmPassword)
+                        }
+                    }
+                    Button{
+                        seeConfirmPassword.toggle()
+                    } label:{
+                        Image(systemName: seeConfirmPassword ? "eye.slash" : "eye")
+                            .foregroundStyle(.gray)
+                    }
+                }
+                .padding()
+                .overlay{
+                    RoundedRectangle(cornerRadius: 25)
+                        .stroke(lineWidth: 1)
+                        .foregroundStyle(.orange)
+                    
+                }
+                
+                //Sign up button
+             Button{
+                 
+             } label: {
+                 ZStack{
+                     RoundedRectangle(cornerRadius: 25)
+                         .fill(.orange)
+                         .frame(width: 350,height:50)
+                     Text("Create Account")
+                         .font(.title2)
+                         .foregroundStyle(.white)
+                         .bold()
+                 }
+             }
+             
+                HStack{
+                    Text("Already have an account?")
+                        .foregroundStyle(.gray)
+                    NavigationLink("Sign in"){
+                        Login()
+                    }
+                    .foregroundStyle(.orange)
+                }
+                
+                //empty view for space
+                HStack{
+                    
+                }
+                .padding(80)
+                
+            }
+            .navigationTitle("Sign Up")
+            .padding()
+        }
+    }
+}
+#Preview {
+    Signup()
+}
+
